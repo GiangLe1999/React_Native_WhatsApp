1858242171940a54662e7da5f8dfdd9c5bb7dcc8
GiangLe1999 <legiangbmt09@gmail.com>
2023-08-20 12:37:40 +0700

    Backend add Authentication with AWS Amplify

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
 import { StyleSheet, View } from "react-native";
 import Navigator from "./navigation";
+import { Amplify } from "aws-amplify";
+import { withAuthenticator } from "aws-amplify-react-native";
+import awsconfig from "./src/aws-exports";
 
-export default function App() {
+Amplify.configure({ ...awsconfig, Analytics: { disabled: true } });
+
+function App() {
   return (
     <View style={styles.container}>
       <Navigator />
@@ -17,3 +22,5 @@ const styles = StyleSheet.create({
     backgroundColor: "whitesmoke",
   },
 });
+
+export default withAuthenticator(App);
